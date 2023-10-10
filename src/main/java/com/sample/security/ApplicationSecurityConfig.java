630d12c4f16ad18662535ac9ff1d650ea934c700
Shailee Gala <shaileegala@gmail.com>
2023-10-09 21:47:24 -0500

    Basic Auth and In memory user

---
+++ src/main/java/com/sample/security/ApplicationSecurityConfig.java
@@ -0,0 +1,95 @@
+package com.sample.security;
+
+import org.springframework.cglib.proxy.NoOp;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig {
+
+    /*private final static List<UserDetails> APPLICATION_USERS = Arrays.asList(
+            new User(
+                    "admin",
+                    "admin",
+                    Collections.singleton(new SimpleGrantedAuthority("admin"))
+            ),
+            new User(
+                    "user",
+                    "user",
+                    Collections.singleton(new SimpleGrantedAuthority("user"))
+            )
+            );
+*/
+
+    @Bean
+    public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> {
+            ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests
+                    .requestMatchers("/","index.html","/css/*","/js/*")
+                    .permitAll()
+                    .anyRequest())
+                    .authenticated();
+                    //.authenticationProvider(authenticationProvider());
+        });
+        http.httpBasic(Customizer.withDefaults());
+        return (SecurityFilterChain)http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        final DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    /*@Bean
+    public UserDetailsService userDetailsService(){
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return APPLICATION_USERS
+                        .stream()
+                        .filter(x -> x.getUsername().equals(username))
+                        .findFirst()
+                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+            }
+        };
+    }*/
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails newUser = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("USER") //"ROLE_USER"
+                .build();
+        return new InMemoryUserDetailsManager(newUser);
+    }
+
+}
